798de0ebb7c1fb1330539475c8029544ff23effd
Ayush_God <ayushgodbole31@gmail.com>
2024-02-12 13:11:10 +0530

    Added custom preloader

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,22 +1,38 @@
+import { useEffect, useState } from 'react';
 import '../../src//index.css';
 import About from './About';
 import Know from './Know';
 import MainSection from './MainSection';
 import Navbar from './Navbar';
 import Skills from './Skills';
+import Pageloader from '../Loader/Pageloader.jsx';
 
 function Homepage(){
-    return (
-        <section className="homepage text-white h-screen" id='home'>
-            <section className='page1'>
-                <Navbar/>
-                <MainSection/>
-                <About/>
-                <Know/>
-                <Skills/>
+
+    // loader state
+    const [loading,setIsloading] = useState(true);
+
+    useEffect(()=>{
+        const timer = ()=>{
+            setTimeout(()=>{
+                setIsloading(false);
+            },700);
+        }
+
+        timer();
+    },[])
+
+    return loading ? <Pageloader /> : (
+            <section className="homepage text-white h-screen" id='home'>
+                <section className='page1'>
+                    <Navbar />
+                    <MainSection />
+                    <About />
+                    <Know />
+                    <Skills />
+                </section>
             </section>
-        </section>
-    )
+        )
 }
 
 export default Homepage;
